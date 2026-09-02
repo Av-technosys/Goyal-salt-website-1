b573180c38a6cca7b04943ac3679aa95033b4724
Nikhil Kumawat <nikhilkumawat002@gmail.com>
2026-09-02 19:00:29 +0530

    feat: Update AGM Notices & Announcements for FY 2025-26; modify metadata and add new documents

--- app/(investors)/notices-announcements/agm/2025-26/page.tsx
+++ app/(investors)/notices-announcements/agm/2025-26/page.tsx
@@ -4,18 +4,16 @@ import { Download, FileText } from "lucide-react";
 export const metadata = {
   title: "AGM Notices & Announcements 2025–26 | Goyal Salt Limited",
   description:
-    "View the Annual General Meeting (AGM) notices and official announcements for FY 2024–25 from Goyal Salt Limited, including shareholder information.",
+    "View the Annual General Meeting (AGM) notices and official announcements for FY 2025–26 from Goyal Salt Limited, including shareholder information.",
   alternates: {
-    canonical: "https://goyalsaltltd.com/notices-announcements/agm/2024-25",
+    canonical: "https://goyalsaltltd.com/notices-announcements/agm/2025-26",
   },
 };
 
 
 const documents = [
-  { title: "GSL_Notice of 15th AGM 2025", pdf: "/Brochure/GSL_Notice-of-15th-AGM.pdf" },
-  { title: "Newspaper Publication_15th AGM", pdf: "/Brochure/Newspaper-Publication_15th-AGM.pdf" },
-  { title: "Proceeding of AGM dated 26.09.2025", pdf: "/Brochure/Proceeding-of-AGM-dated-26.09.2025.pdf" },
-  { title: "Voting Result and Scrutinizer's Report", pdf: "/Brochure/Voting-Result-and-Scrutinizers-Report.pdf" },
+  { title: "GSL_Notice of 16th AGM 2025", pdf: "/Brochure/GSL_Notice_of_16th_AGM.pdf" },
+  { title: "Annual Report 2025-26", pdf: "/Brochure/Annual_Report_2025-26.pdf" },
  
 ];
 
@@ -33,13 +31,11 @@ const Page = () => {
           <p className="mt-4 text-xl text-black max-w-xl mx-auto">
             View our latest investor documents and financial reports.
           </p>
-          <p className="mt-4 text-xl text-gray-500 max-w-xl mx-auto">
-           Pdf not found.
-          </p>
+          
         </div>
 
         {/* Cards */}
-        {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {documents.map((doc, index) => (
             <a
               key={index}
@@ -65,7 +61,7 @@ const Page = () => {
               </h3>
             </a>
           ))}
-        </div> */}
+        </div>
       </div>
     </section>
   );
